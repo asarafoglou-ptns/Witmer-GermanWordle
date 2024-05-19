0861429f63709dc65b4345b2eaeaed01b109ee37
jessiwitti <123743330+jessiwitti@users.noreply.github.com>
2024-05-19 18:44:14 +0200

    Version with example in it
    A helpfile version with example in it

--- GermanWordle/R/Week2functions.R
+++ GermanWordle/R/Week2functions.R
@@ -6,18 +6,13 @@ source("Wordlist.R")
 #' @export
 target <- sample(german_words, 1)
 
-
-
 #' @title Compare Words
 #' @description This function compares whether the letters in the target word are the same as in the guessed word
 #' @param target_str character. Should be a word with the same length as the guessed word
 #' @param guess_str character. Should be a word with the same length as the target word
 #' @return Shows whether the letter is in the word and on the correct place, in the word but incorrect place, or not in the word
 #' @examples
-#' # compare_words("rauch","hallo")
-#' 
-#' # Compare two words
-#' compare_words("rauch","hallo")
+#' # compare_words("haus","maus")
 #' @export
 compare_words <- function(target_str, guess_str) {
   if (nchar(target_str) != nchar(guess_str)) {
